9d7c16ba0c69c475885d13bcbc484bd1edb76666
Stephen Colebourne <scolebourne@joda.org>
2020-07-06 14:59:33 +0100

    Use status check

--- index.js
+++ index.js
@@ -3,15 +3,8 @@ const github = require('@actions/github');
 
 const validEvent = ['pull_request'];
 
+const GROUP_PR_TITLE = 'ogbot PR title';
 const JIRA_PATTERN = /^([A-Z]+-\d+): .*$/;
-
-function isDependabot(user) {
-  return user === 'dependabot[bot]' || user === 'dependabot-preview[bot]';
-}
-
-function isOgbot(user) {
-  return user === 'opengammacibot';
-}
 
 async function run() {
     try {
@@ -28,30 +21,32 @@ async function run() {
         const repo = github.context.payload.pull_request.base.repo.name;
 
 		// ensure pull request info is up to date
-        const {data: pullRequest} = await client.pulls.get({
+        const {data: pr} = await client.pulls.get({
           owner,
           repo,
           pull_number: github.context.payload.pull_request.number
         });
-        const prUser = pullRequest.user.login;
-        const prTitle = pullRequest.title;
-        core.info(`Pull Request ${repo}/${pullRequest.number} has title: "${prTitle}"`);
+        const prUser = pr.user.login;
+        const prTitle = pr.title;
+        core.info(`Pull Request ${owner}/${repo}/${pr.number} has title: "${prTitle}"`);
 
         // validate
         if (isDependabot(prUser) || isOgbot(prUser)) {
           core.info("JIRA: PR is from dependabot/ogbot");
+		  updateStatus(client, owner, repo, pr, GROUP_PR_TITLE, "success", "No need to check JIRA format - PR is from dependabot/ogbot");
         } else if (prTitle.indexOf('WIP')>= 0) {
           core.info("JIRA: Branch is WIP");
-		  core.setFailed("PR title indicates WIP (Work In Progress)");
+		  updateStatus(client, owner, repo, pr, GROUP_PR_TITLE, "pending", "Work In Progress - change PR title to enable merging");
         } else if (prTitle.startsWith('TASK: ')) {
           core.info("JIRA: TASK prefix found");
+		  updateStatus(client, owner, repo, pr, GROUP_PR_TITLE, "success", "WARNING! Use of 'TASK' prefix is not currently recommended");
         } else if (JIRA_PATTERN.test(prTitle)) {
           core.info("JIRA: Valid JIRA format found");
+		  updateStatus(client, owner, repo, pr, GROUP_PR_TITLE, "success", "PR title contains JIRA reference");
         } else {
           core.info("JIRA: Invalid title found with no JIRA");
-		  core.setFailed("PR title does not start with JIRA reference, eg. 'PROD-123: '");
+		  updateStatus(client, owner, repo, pr, GROUP_PR_TITLE, "failure", "PR title does not start with JIRA reference, eg. 'PROD-123: '");
         }
-		return;
 
     } catch (error) {
         core.setFailed(error.message);
@@ -59,3 +54,44 @@ async function run() {
 }
 
 run();
+
+//------------------------------------------------------
+// checks and updates the status
+async function updateStatus(client, owner, repo, pr, group, state, msg) {
+  // query the current status and check if we need to make a change
+  // this avoids repeatedly updating the status when it hasn't changed
+  // (GitHub stores each update immutably as a new status)
+  const {data: current} = await client.repos.listStatusesForRef({
+    owner,
+    repo,
+    ref: pr.head.sha
+  });
+  
+//  const current = (await github.repos.listStatusesForRef(context.repo({ref: pr.head.sha}))).data;
+  const matched = current.find(status => status.context === group);
+  if (matched && matched.state === state && matched.description === msg) {
+    core.info("No need to update status on GitHub");
+    return;
+  }
+  // update the status
+  core.info("Update status on GitHub to " + state);
+  return client.repos.createStatus({
+    owner,
+    repo,
+    sha: pr.head.sha,
+    state: state,
+    description: msg,
+    context: group
+  });
+}
+
+//------------------------------------------------------
+function isDependabot(user) {
+  return user === 'dependabot[bot]' || user === 'dependabot-preview[bot]';
+}
+
+//------------------------------------------------------
+function isOgbot(user) {
+  return user === 'opengammacibot';
+}
+
